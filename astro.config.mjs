468d2fd42cc26717b6db46f0a741c1cdc3e86aee
3dwardperezs <3dwardperezs@gmail.com>
2024-09-08 21:31:09 +0100

    changes 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   integrations: [
@@ -8,4 +9,5 @@ export default defineConfig({
     svelte(),
   ],
   output: "server",
+  adapter: netlify()
 });
