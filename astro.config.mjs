c81084e69dd77e88bf6ca8e4ec87f75b2f665931
3dwardperezs <3dwardperezs@gmail.com>
2024-09-08 20:52:30 +0100

    changes 1

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    svelte(),
+  ],
+  output: "server",
+});
---
+++ src/components/IconHelp.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from 'svelte';
+    let iconVisible = true;
+  
+    onMount(() => {
+        const timer = setInterval(() => {
+          iconVisible = !iconVisible;
+        }, 1000); 
+        return () => clearInterval(timer);
+    });
+</script>
+
+<div class="icon-container">
+    <a href="/ContactPage" aria-label="Contact Us" class={iconVisible ? 'block' : 'hidden'}>
+      <svg width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-whatsapp"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
+    </a>
+</div>
+  
