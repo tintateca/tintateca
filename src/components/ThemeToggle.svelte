fe6a1f4385203d9860bec4d684b42120b40b4ce0
3dwardperezs <3dwardperezs@gmail.com>
2024-09-11 19:59:11 +0100

    changes 5

---
+++ src/components/ThemeToggle.svelte
@@ -0,0 +1,19 @@
+<script>
+    let isGreen = true;
+
+    function toggleColor() {
+        const body = document.body;
+        if (isGreen) {
+            body.classList.remove('bg-green-600');
+            body.classList.add('bg-orange-600');
+        } else {
+            body.classList.remove('bg-orange-600');
+            body.classList.add('bg-green-600');
+        }
+        isGreen = !isGreen;
+    }
+</script>
+
+<button on:click={toggleColor} class="bg-white text-black p-2 rounded">
+    Toggle Background Color
+</button>
